3ab8ce76f380930a55ea478f431051c1e4e06afe
Ole <ole.ersoy@gmail.com>
2018-09-11 08:35:51 -0500

    fixed forms.njk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
- let fs = require('fs');
- let nj = require('nunjucks');
- var contact = fs.readFileSync('./src/contact.html','utf8');
- nj.configure('src');
- let result = nj.render('contact.html', function(err, res) {
+let fs = require('fs');
+let nj = require('nunjucks');
+var contact = fs.readFileSync('./src/contact.html','utf8');
+nj.configure('src');
+let result = nj.render('contact.html', function(err, res) {
 console.log(err)});
 console.log(result);
